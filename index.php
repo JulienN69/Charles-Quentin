747b7b7b49eed9da83d06325688a8e93556571c0
Julien Nesme <nesmejulien@yahoo.fr>
2024-02-14 22:15:12 +0100

    back to last index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 require 'vendor/autoload.php';
-require 'App/Controller/Controller.php';
-
 
 // spl_autoload_register();
 
@@ -9,9 +7,7 @@ define('_ROOTPATH_', __DIR__);
 define('BASE_URL', 'https://charles-cantin-nesme-f6af6cc1162d.herokuapp.com/');
 // define('BASE_URL', 'http://localhost/charles_cantin');
 
-// use App\Controller\Controller;
-
-// $controller = '\\' . str_replace(DIRECTORY_SEPARATOR, '\\', $controller);
+use App\Controller\Controller;
 
 $controller = new Controller();
 $controller->route();
