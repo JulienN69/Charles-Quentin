3c1db8dc3a81cf48ea9544124f32f14529f2124c
Julien Nesme <nesmejulien@yahoo.fr>
2024-02-14 22:37:57 +0100

    change index

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 // spl_autoload_register();
 
 define('_ROOTPATH_', __DIR__);
-define('BASE_URL', 'https://charles-cantin-nesme-f6af6cc1162d.herokuapp.com/');
+define('BASE_URL', 'https://vast-coast-14611-21c322dd0d31.herokuapp.com/');
 // define('BASE_URL', 'http://localhost/charles_cantin');
 
 use App\Controller\Controller;
