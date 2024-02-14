2a3a3649c4633dd09cd6efb55e02b746975cfba0
Julien Nesme <nesmejulien@yahoo.fr>
2024-02-14 22:04:20 +0100

    test require instead use in index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 require 'vendor/autoload.php';
+require 'App/Controller/Controller.php';
+
 
 // spl_autoload_register();
 
@@ -7,14 +9,9 @@ define('_ROOTPATH_', __DIR__);
 define('BASE_URL', 'https://charles-cantin-nesme-f6af6cc1162d.herokuapp.com/');
 // define('BASE_URL', 'http://localhost/charles_cantin');
 
-use App\Controller\Controller;
+// use App\Controller\Controller;
 
-// $controller = '\\' . str_replace(DIRECTORY_SEPARATOR, '\\', $controller);
+$controller = '\\' . str_replace(DIRECTORY_SEPARATOR, '\\', $controller);
 
-// $controller = new Controller();
-// $controller->route();
-
-$controllerNamespace = '\\' . str_replace(DIRECTORY_SEPARATOR, '\\', 'App\Controller\Controller');
-
-$controller = new $controllerNamespace();
+$controller = new Controller();
 $controller->route();
