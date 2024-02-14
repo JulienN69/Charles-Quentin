15298af89c21c72cabe86f645e76a43466c13c3d
Julien Nesme <nesmejulien@yahoo.fr>
2024-02-14 22:33:34 +0100

    reboot autoloader

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ define('_ROOTPATH_', __DIR__);
 define('BASE_URL', 'https://charles-cantin-nesme-f6af6cc1162d.herokuapp.com/');
 // define('BASE_URL', 'http://localhost/charles_cantin');
 
-use App/Controller/Controller;
+use App\Controller\Controller;
 
 $controller = new Controller();
 $controller->route();
