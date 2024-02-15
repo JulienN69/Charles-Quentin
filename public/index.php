7b2a4a8ad87340a4cf97a27490735ca495020828
Julien Nesme <nesmejulien@yahoo.fr>
2024-02-15 23:17:51 +0100

    test 12

--- public/index.php
+++ public/index.php
@@ -1,5 +1,21 @@
 <?php
 require '../vendor/autoload.php';
+
+define('_ROOTPATH_', __DIR__ . '/../');
+
+define('BASE_URL', 'https://charles-cantin-nesme-f6af6cc1162d.herokuapp.com/');
+
+use App\Controller\Controller;
+
+
+$controller = new Controller();
+$controller->route();
+
+
+// define('BASE_URL', 'http://localhost/charles_cantin');
+
+// spl_autoload_register();
+
 // require 'App/Controller/Controller.php';
 // spl_autoload_register(function ($class) {
 //     $class = str_replace('\\', '/', $class);
@@ -12,25 +28,3 @@ require '../vendor/autoload.php';
 //         require_once $filePath;
 //     }
 // });
-
-echo 'page chargé';
-
-define('_ROOTPATH_', __DIR__);
-define('BASE_URL', 'https://charles-cantin-nesme-f6af6cc1162d.herokuapp.com/');
-
-
-use App\Controller\Controller;
-
-echo __DIR__ . '/App/Controller/Controller.php';
-echo '------------';
-
-var_dump(file_get_contents(__DIR__ . '/vendor/autoload.php'));
-
-
-$controller = new Controller();
-$controller->route();
-
-
-// define('BASE_URL', 'http://localhost/charles_cantin');
-
-// spl_autoload_register();
