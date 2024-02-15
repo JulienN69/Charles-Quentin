a003d21b4d4edb05352a16cb250f1a299589db7f
Julien Nesme <nesmejulien@yahoo.fr>
2024-02-15 22:28:08 +0100

    test 4

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
-require 'vendor/autoload.php';
+// require 'vendor/autoload.php';
 // require 'App/Controller/Controller.php';
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', '/', $class);
+
+    $rootPath = __DIR__ . '/app/';
+
+    $filePath = $rootPath . $class . '.php';
+
+    if (file_exists($filePath)) {
+        require_once $filePath;
+    }
+});
 
 echo 'page chargé';
 
@@ -12,7 +23,7 @@ use App\Controller\Controller;
 $controller = new Controller();
 echo 'Controller chargé';
 var_dump($controller) ;
-$controller->route();
+// $controller->route();
 
 
 // define('BASE_URL', 'http://localhost/charles_cantin');
