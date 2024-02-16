4d8dda1e57d792d8178f004fb3c21391fb6fc1e1
Julien Nesme <nesmejulien@yahoo.fr>
2024-02-16 10:49:58 +0100

    change balise base

--- template/header.php
+++ template/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <base href="/charles_cantin/">
+        <base href="https://charles-cantin-nesme-f6af6cc1162d.herokuapp.com/">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet">
